cd1c5457fed6f1499ecbe0869e01319a8be3f51f
Neil Sambhu <neilsambhu@gmail.com>
2020-04-04 23:40:29 -0400

    file IO

--- BA_I3D.py
+++ BA_I3D.py
@@ -3,11 +3,12 @@ import os
 
 dir_I3D = "./bf_kinetics_feat/"
 dir_labels = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/"
+actions = ["cut","walk_in","spoon","peel","stir","walk_out", "smear","put","squeeze", "butter", "pour", "fry", "crack", "take", "add"]
 
 def LoadData():
 	X, y = np.array([]), np.array([])
 	for feature in os.listdir(dir_labels):
-		print(feature)
+		print(feature.split('_'))
 		break
 
 def main():
