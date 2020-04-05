0019e9970af77dfa5a2665d875feb97af40ab833
Neil Sambhu <neilsambhu@gmail.com>
2020-04-04 23:54:54 -0400

    file IO

--- BA_I3D.py
+++ BA_I3D.py
@@ -3,14 +3,19 @@ import os
 
 dir_I3D = "./bf_kinetics_feat/"
 dir_labels = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/"
-actions = ["cut","walk_in","spoon","peel","stir","walk_out", "smear","put","squeeze", "butter", "pour", "fry", "crack", "take", "add"]
+
+class Actions(Enum):
+	cut
+	walk_in
+	put
+	# ","spoon","peel","stir","walk_out", "smear","squeeze", "butter", "pour", "fry", "crack", "take", "add"]
 
 def LoadData():
 	X, y = np.array([]), np.array([])
 	for feature in os.listdir(dir_labels):
 		featureSplit = feature.split('_')
 		if len(featureSplit) > 1:
-			print(featureSplit[4])
+			print(Actions[featureSplit[4]])
 		# break
 
 def main():
