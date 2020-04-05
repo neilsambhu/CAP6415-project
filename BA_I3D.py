bf2a949e355e40d2da04c9c4af41053031a917a3
Neil Sambhu <neilsambhu@gmail.com>
2020-04-04 23:11:57 -0400

    start file IO

---
+++ BA_I3D.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+dir_I3D = "./bf_kinetics_feat/"
+dir_labels = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/"
+
+def LoadData():
+	X, y = np.array([]), np.array([])
+	for feature in os.listdir(dir_labels):
+		print(feature)
+
+def main():
+	LoadData()
+
+if __name__ == '__main__':
+	main()
