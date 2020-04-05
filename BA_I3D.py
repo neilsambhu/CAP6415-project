8f5f0fb94209236ccaf665fa7c99c862d2ea29ad
Neil Sambhu <neilsambhu@gmail.com>
2020-04-05 00:01:42 -0400

    file IO

--- BA_I3D.py
+++ BA_I3D.py
@@ -6,9 +6,10 @@ dir_I3D = "./bf_kinetics_feat/"
 dir_labels = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/"
 
 class Actions(Enum):
-	cut=0
-	walk_in
-	put
+	put=0
+	# cut=0
+	# walk_in
+	# put
 	# ","spoon","peel","stir","walk_out", "smear","squeeze", "butter", "pour", "fry", "crack", "take", "add"]
 
 def LoadData():
