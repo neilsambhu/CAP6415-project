7534ec5f6ac9a130a3c5689c31de59f83e76cb45
Neil Sambhu <neilsambhu@gmail.com>
2020-04-08 20:33:58 -0400

    FV file IO

--- BA_FV.py
+++ BA_FV.py
@@ -15,7 +15,7 @@ def LoadData():
 		for file in os.listdir(dir_objects):
 			print(file)
 			temp = np.loadtxt(os.path.join(dir_objects,file))
-			print(temp)
+			print(temp[:,1:])
 			print(temp[:,1:].shape)
 			break
 		break
