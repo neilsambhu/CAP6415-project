0ba171c42826c2c928c49a687107f372a46e0956
Neil Sambhu <neilsambhu@gmail.com>
2020-04-12 16:49:27 -0400

    train-test split

--- BA_FV.py
+++ BA_FV.py
@@ -51,7 +51,10 @@ def ML_Classifier(x_test,y_test,
 
 def main():
 	x_test,y_test,x_train,y_train = LoadData()
-	ML_Classifier(x_test,y_test,x_train,y_train)
+	print('10 trees')
+	ML_Classifier(x_test,y_test,x_train,y_train,10)
+	print('100 trees')
+	ML_Classifier(x_test,y_test,x_train,y_train,100)
 
 if __name__ == '__main__':
 	main()
