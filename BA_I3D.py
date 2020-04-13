1668ff30954b5af1dba3668daefd6454635910a7
Neil Sambhu <neilsambhu@gmail.com>
2020-04-13 15:29:05 -0400

    load data fix for I3D actions

--- BA_I3D.py
+++ BA_I3D.py
@@ -67,7 +67,7 @@ def LoadData2():
 				dir_cam,'*.labels')
 			# print(filesLabelsRegex)
 			for fileLabels in glob.glob(filesLabelsRegex):
-				print(fileLabels.split('_')[-1])
+				print(fileLabels.split('_')[-1].split('.')[0])
 				with open(fileLabels) as fp:
 					line = fp.readline()
 					while line:
