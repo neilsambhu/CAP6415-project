cbd10753d74e625618c64df3adb2b655d7db3ece
Neil Sambhu <neilsambhu@gmail.com>
2020-04-17 17:52:22 -0400

    pretty confusion matrix

--- BA_I3D.py
+++ BA_I3D.py
@@ -10,55 +10,8 @@ from sklearn.utils import shuffle
 dir_I3D = "./bf_kinetics_feat/"
 dir_labels = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/"
 dir_labels = '/home/DATASETS/BreakfastII_15fps_qvga_sync/'
-# def LoadData():
-# 	x_test, y_test = list(), list()
-# 	x_train, y_train = list(), list()
-# 	for feature in tqdm(os.listdir(dir_labels)):
-# 		featureSplit = feature.split('_')
-# 		if len(featureSplit) <= 1:
-# 			continue
-# 		# replace 'salad' with 'salat'
-# 		if featureSplit[2] == 'salad':
-# 			featureSplit[2] = 'salat'
-# 		filePath_x = os.path.join(dir_I3D,
-# 			featureSplit[1] + '_' + 
-# 			featureSplit[3] + '_' +
-# 			featureSplit[1] + '_' + 
-# 			featureSplit[2] + '.npy')
-# 		if not os.path.exists(filePath_x):
-# 			print(f'File not found: {filePath_x}')
-# 			continue
-# 		featureVideo = np.load(filePath_x)
-# 		# print(featureVideo.shape)
-# 		frameOffset = 5
-# 		frameStart = int(featureSplit[-2])-frameOffset #need to do the check here
-# 		if frameStart < 0:
-# 			frameStart = 0
-# 		frameEnd = int(featureSplit[-1])-frameOffset+1
-# 		frameCount = frameEnd-frameStart
-# 		if frameEnd > featureVideo.shape[0]:
-# 			print(f'Error: frameEnd value {frameEnd}', 
-# 				f'featureVideo.shape[0] {featureVideo.shape[0]}')
-# 			print(featureSplit)
-# 			quit()
-# 			continue
-# 		# print(frameEnd,frameStart,featureSplit[7],featureSplit[6],
-# 		# 	frameEnd-frameStart,int(featureSplit[7])-int(featureSplit[6]))
-# 		feature_x = featureVideo[frameStart:frameEnd,:]
-# 		# print(feature_x.shape)
-# 		# test
-# 		if int(featureSplit[1][1:]) < 16:
-# 			x_test.extend(feature_x)
-# 			y_test.extend([featureSplit[4]] * frameCount)
-# 		# train
-# 		else:
-# 			x_train.extend(feature_x)
-# 			y_train.extend([featureSplit[4]] * frameCount)
-# 	print(np.array(x_test).shape,np.array(y_test).shape,
-# 		np.array(x_train).shape,np.array(y_train).shape)
-# 	return np.array(x_test),np.array(y_test), \
-# 		np.array(x_train),np.array(y_train)
-def LoadData2():
+
+def LoadData():
 	x_test, y_test = list(), list()
 	x_train, y_train = list(), list()
 	# x_test, y_test = np.array([]), np.array([])
@@ -208,47 +161,32 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
             print(cell, end=" ")
         print()
 def ML_Classifier(x_test,y_test,
-	x_train,y_train,n_estimators=1):
+	x_train,y_train,labels,n_estimators=1):
 	clf = RandomForestClassifier(n_estimators=n_estimators)
-	# k = 1000
-	# from collections import Counter
-	# print(Counter(y).keys())
-	# print(Counter(y).values())
-	# clf.fit(x[:k,:],y[:k])
 	clf.fit(x_train,y_train)
-	# for item in clf.feature_importances_:
-	# 	print(item, end=" ")
-	
-	# y_true = y[:k]
-	# y_pred = clf.predict(x[:k,:])
+
 	y_true = y_test
 	y_pred = clf.predict(x_test)
-	print(accuracy_score(y_true,y_pred))
-	labels = np.unique(y_train)
-	print(labels)
-	confusionMatrix = confusion_matrix(y_true,y_pred, labels=labels)
+	print(f'acc: {accuracy_score(y_true,y_pred)}')
+	confusionMatrix = confusion_matrix(y_true,y_pred,labels=labels)
 	print_cm(confusionMatrix,labels)
 	print(f'confusion matrix shape {confusionMatrix.shape}')
-def strToInt(y):
-	le = preprocessing.LabelEncoder()
-	return le.fit_transform(y)
+
 def main():
-	x_test,y_test,x_train,y_train = LoadData2()
-	# y_test = strToInt(y_test)
-	# y_train = strToInt(y_train)
+	x_test,y_test,x_train,y_train = LoadData()
+	labels = np.unique(y_train)
 	
-	# use 1% of training data
+	# use 10% of training data
 	n_samples = x_train.shape[0] // 100
-	# print(random.sample(zip(x_train,y_train), itemCount))
 	x_train,y_train = shuffle(x_train,y_train, 
 		n_samples=n_samples)
 
 	print('1 tree')
-	ML_Classifier(x_test,y_test,x_train,y_train)
+	ML_Classifier(x_test,y_test,x_train,y_train,labels)
 	# print('10 trees')
-	# ML_Classifier(x_test,y_test,x_train,y_train,10)
+	# ML_Classifier(x_test,y_test,x_train,y_train,labels,10)
 	# print('100 trees')
-	# ML_Classifier(x_test,y_test,x_train,y_train,100)
+	# ML_Classifier(x_test,y_test,x_train,y_train,labels,100)
 
 if __name__ == '__main__':
 	main()
