a2df6cdfe4ee5340dfb811fca5fbd01b3b31cdcb
Neil Sambhu <neilsambhu@gmail.com>
2020-04-20 18:51:53 -0400

    load RF model from disk

--- BA_FV.py
+++ BA_FV.py
@@ -54,24 +54,26 @@ def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=
             print(cell, end=" ")
         print()
 def ML_Classifier(x_test,y_test,
-	x_train,y_train,n_estimators=1):
-	clf = RandomForestClassifier(n_estimators=n_estimators)
-	clf.fit(x_train,y_train)
-	dump(clf, f'output/FV-01-RF-{n_estimators}.joblib')
+	x_train,y_train,labels,n_estimators=1):
+	# clf = RandomForestClassifier(n_estimators=n_estimators)
+	# clf.fit(x_train,y_train)
+	# dump(clf, f'output/FV-01-RF-{n_estimators}.joblib')
+	clf = load(f'output/FV-01-RF-{n_estimators}.joblib')
 
 	y_true = y_test
 	y_pred = clf.predict(x_test)
 	print(f'acc: {accuracy_score(y_true,y_pred)}')
-	confusionMatrix = confusion_matrix(y_true,y_pred)
+	confusionMatrix = confusion_matrix(y_true,y_pred,labels=labels)
 	print_cm(confusionMatrix,labels)
 	print(f'confusion matrix shape {confusionMatrix.shape}')
 
 def main():
 	x_test,y_test,x_train,y_train = LoadData()
+	labels = np.unique(y_train)
 	# print('10 trees')
-	# ML_Classifier(x_test,y_test,x_train,y_train,10)
+	# ML_Classifier(x_test,y_test,x_train,y_train,labels,10)
 	print('100 trees')
-	ML_Classifier(x_test,y_test,x_train,y_train,100)
+	ML_Classifier(x_test,y_test,x_train,y_train,labels,100)
 
 if __name__ == '__main__':
 	main()
