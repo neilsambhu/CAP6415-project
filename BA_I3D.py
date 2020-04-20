eac96d028c7275c2455cfffaeec20c5a4392a499
Neil Sambhu <neilsambhu@gmail.com>
2020-04-20 19:32:35 -0400

    file paths

--- BA_I3D.py
+++ BA_I3D.py
@@ -98,9 +98,9 @@ def LoadData():
 						# 	quit()
 						line = fp.readline()
 	
-	x_test,y_test,listFileName_test,
-	x_train,y_train = np.array(x_test), 
-		np.array(y_test),np.array(listFileName_test), 
+	x_test,y_test,listFileName_test, \
+	x_train,y_train = np.array(x_test), \
+		np.array(y_test),np.array(listFileName_test), \
 		np.array(x_train),np.array(y_train)
 	print(x_test.shape,y_test.shape,listFileName_test.shape, 
 		x_train.shape,y_train.shape)
