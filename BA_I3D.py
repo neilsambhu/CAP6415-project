7cfe9beaa32adb18c1093067b8e2a62f3c87cb3c
Neil Sambhu <neilsambhu@gmail.com>
2020-04-21 20:01:04 -0400

    dict HOF file paths

--- BA_I3D.py
+++ BA_I3D.py
@@ -219,7 +219,7 @@ def create_file_structure(fileName_Features):
 		# single file path
 		predict_proba = clf.predict_proba(arrFeatures)
 		print(predict_proba.shape)
-		print(predict_proba)
+		print(predict_proba[0])
 
 		# print(predict_proba[0])
 		quit()
