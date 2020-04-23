d1ddfd15bbf93b1bdc111720671adad4ea011344
Neil Sambhu <neilsambhu@gmail.com>
2020-04-22 22:28:29 -0400

    HOF-HOG acc

---
+++ HOF_HOG_acc.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os, glob, re
+from tqdm import tqdm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import preprocessing
+from sklearn.utils import shuffle
+from joblib import dump, load
+np.set_printoptions(suppress=True)
+
+dir_HOF_HOG = "./PatternTheory_WACV_Original/PatternTheory_WACV_Original/S1_PreProcessFiles/labels/"
+
+def labelAccuracy(filepathRegex):
+	for filepath in glob.glob(filepathRegex):
+		print(filepath)
+
+def main():
+	labelAccuracy(dir_HOF_HOG + 'HOF*')
+
+if __name__ == '__main__':
+	main()
